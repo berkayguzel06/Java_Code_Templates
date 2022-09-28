c361f415ab8b94a9084ace48a1e7029e98c0eb11
Berkay Güzel <98205992+cyberhacker006@users.noreply.github.com>
2022-09-28 14:25:04 +0300

    Update SortAlgorithm.java

--- SortAlgorithm.java
+++ SortAlgorithm.java
@@ -32,4 +32,41 @@ public class SortAlgorithm {
 			System.out.println(arr[i]);
 		}
 	}
+	//Sort two sorted array
+	static public void mergeSort(int arr1[],int arr2[]) {	
+		int lenght = arr1.length+arr2.length;
+		int full[] = new int[lenght];
+		int count1=0;
+		int count2=0;
+		int count3=0;
+		for(int i=0;i<lenght;i++) {
+			if(count2>=arr2.length) {
+				for(int j=count1;j<arr1.length;j++) {
+					full[count3]=arr1[j];
+					count3++;
+				}
+				break;
+			}
+			else if(count1>=arr1.length) {
+				for(int j=count2;j<arr2.length;j++) {
+					full[count3]=arr2[j];
+					count3++;
+				}
+				break;
+			}
+			else if(arr1[count1]<arr2[count2]) {
+				full[count3]=arr1[count1];
+				count1++;
+				count3++;
+			}
+			else if(arr1[count1]>arr2[count2]) {
+				full[count3]=arr2[count2];
+				count2++;
+				count3++;
+			}	
+		}
+		for(int i=0;i<full.length;i++) {
+			System.out.println(full[i]);
+		}
+	}
 }
